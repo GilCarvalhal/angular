cbf7a4991dd888ab8eed2bd61adf85ec8b38ecb0
Casa <gilbertocarvalhalmatos@gmail.com>
2023-05-22 10:57:23 -0300

    Criando nosso primeiro Modulo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './componentes/home/home.component';
 import { NovoComponent } from './componentes/novo/novo.component';
+import { ProdutoModule } from './features/produto/produto.module';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'novo', component: NovoComponent },
+  // Lazy-loading
+  {
+    path: 'produto',
+    loadChildren: () => import('./features/produto/produto.module').then(m => m.ProdutoModule),
+  },
 ];
 
 @NgModule({
