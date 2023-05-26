4ca1ecdd6152f963787216718223aa7348bc5955
Casa <gilbertocarvalhalmatos@gmail.com>
2023-05-25 22:46:00 -0300

    Criando recursos com JsonServer

--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -18,32 +18,7 @@ export interface Pessoas extends Array<Pessoa> {}
 export class HomeComponent {
   title = null;
 
-  clientes: Pessoas = [
-    {
-      nome: 'Alexandre',
-      sexo: 'M',
-      idade: 20,
-      salario: '10000',
-    },
-    {
-      nome: 'Roberto',
-      sexo: 'M',
-      idade: 22,
-      salario: '11000',
-    },
-    {
-      nome: 'Maura',
-      sexo: 'F',
-      idade: 23,
-      salario: '12000',
-    },
-    {
-      nome: 'Claudia',
-      sexo: 'f',
-      idade: 21,
-      salario: '13000',
-    },
-  ];
+  clientes!: Pessoas;
 
   displayedColumns: string[] = ['nome', 'sexo', 'idade', 'salario'];
   dataSource!: MatTableDataSource<any>;
