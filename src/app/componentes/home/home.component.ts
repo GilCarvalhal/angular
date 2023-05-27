d92cbdceea3717388768b324d7d75e5f3f52ae80
Casa <gilbertocarvalhalmatos@gmail.com>
2023-05-26 23:20:04 -0300

    HTTP Get e JsonServer

--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { map, of, tap } from 'rxjs';
+import { HomeService } from '../services/home.service';
 
 export interface Pessoa {
   nome: string;
@@ -21,21 +22,21 @@ export class HomeComponent {
 
   // Pipe do rxjs !== Angular Pipe
 
-  frutas: string[] = [];
+  // frutas: string[] = [];
 
   // Dessa forma, com o '$' por convenção se fala que isso é um observable.
-  frutas$ = of('banana', 'morango', 'abacaxi', 'pera', 'melancia');
+  // frutas$ = of('banana', 'morango', 'abacaxi', 'pera', 'melancia');
 
-  constructor() {
-    this.frutas$
-      .pipe(
-        tap(console.log),
-        map((fruta) => fruta.toUpperCase()),
-        tap(console.log)
-      )
-      .subscribe((resultado) => {
-        this.frutas.push(resultado);
-      });
+  constructor(private homeService: HomeService) {
+    // this.frutas$
+    //   .pipe(
+    //     tap(console.log),
+    //     map((fruta) => fruta.toUpperCase()),
+    //     tap(console.log)
+    //   )
+    //   .subscribe((resultado) => {
+    //     this.frutas.push(resultado);
+    //   });
   }
 
   clientes!: Pessoas;
@@ -43,6 +44,9 @@ export class HomeComponent {
   displayedColumns: string[] = ['nome', 'sexo', 'idade', 'salario'];
   dataSource!: MatTableDataSource<any>;
   ngOnInit(): void {
-    this.dataSource = new MatTableDataSource(this.clientes);
+    this.homeService.getClientes().subscribe((clientes) => {
+      this.clientes = clientes;
+      this.dataSource = new MatTableDataSource(this.clientes);
+    });
   }
 }
